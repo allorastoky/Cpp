bbb1769c40de2b75bc3647c19dd346ec442a33d7
Gianluca Ghinazzi <gianluca.ghinazzi@gmail.com>
2019-12-01 16:15:32 +0100

    Terminata sezione stampa vettori e quasi completata quella riguardo la loro gestione

--- Main.cpp
+++ Main.cpp
@@ -4,7 +4,15 @@
 using namespace std;
 
 int main(){
-	
-	float a= randomNM_float(2,47);
-	cout << a;
+	int n=20;
+	int v[n];
+	riempiVettoreRandom(v, n);
+	for(int i=0; i<n; i++){
+		cout << v[i] <<" ";
+	}
+	cout << endl;
+	rimuoviElemento(v, n, 13);
+	for(int i=0; i<n; i++){
+		cout << v[i] <<" ";
+	}
 }
--- library.h
+++ library.h
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <vector>
 
 using namespace std;
-
-int random_min=0; int random_max=100;
-
+int random_min=0; int random_max=100; int dim_max=1000;
 int randomNM(int n, int m) {
 	srand(time(NULL));
 	int numb=rand()%(m-n+1)+n;
@@ -35,3 +34,45 @@ void setRandomMin(int min) {
 void setRandomMax(int max) {
 	random_max= max;
 }
+void setDimensioneMax(int x) {
+	dim_max=x;
+}
+void riempiVettore(int v[], int n) {
+	int a;
+	for(int i=0; i<n; i++){
+		cout << "Che valore desideri aggiungere al tuo vettore (solo interi)? ";
+		cin >> a;
+		v[i]=a;
+		cout << endl << "Perfetto," << "l'elemento '" << a << "' è stato aggiunto al vettore!"<< endl;
+	}
+}
+void riempiVettore(float v[], int n) {
+	float a;
+	for(int i=0; i<n; i++){
+		cout << "Che valore desideri aggiungere al tuo vettore? ";
+		cin >> a;
+		v[i]=a;
+		cout << endl << "Perfetto," << "l'elemento '" << a << "' è stato aggiunto al vettore!"<< endl;
+	}
+}
+void riempiVettoreRandom(int v[], int n) {
+	for(int i=0; i<n; i++){
+		v[i]= rand()%(random_max-random_min+1)+random_min;
+	}
+}
+void riempiVettoreRandom(float v[], int n) {
+	for(int i=0; i<n; i++){
+		v[i]=random_min+(float(rand())/float(RAND_MAX/(random_max-random_min)));
+	}
+}
+void stampaVettore(float v[], int n){
+	for(int i=0; i<n; i++){
+		cout << v[i] <<" ";
+	}
+}
+void stampaVettore(int v[], int n){
+	for(int i=0; i<n; i++){
+		cout << v[i] <<" ";
+	}
+}
+
